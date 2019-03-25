efb11719ec4faac2be9be0c6e3a488cdc2a9ef33
Viktor Ermakov <ermakov.viktor98@gmail.com>
2019-03-25 21:35:25 +0300

    v 1.0

---
+++ src/components/Todos.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
+
+class Todos extends Component {
+  render() {
+    const { todos, markComplete, delTodo } = this.props;
+
+    return todos.map(todo => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        markComplete={markComplete}
+        delTodo={delTodo}
+      />
+    ));
+  }
+}
+
+Todos.propTypes = {
+  todos: PropTypes.arrayOf(PropTypes.object).isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired
+};
+
+export default Todos;
